c36f9ff224cd29941ed13c044405405781a0706e
Ayushman <redsteel200.as@gmail.com>
2024-08-22 20:57:51 +0530

    all the functionality created, css needed only

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+
 
 function App() {
+const url="https://api.api-ninjas.com/v1/quotes?"
+
+
+const [quote,setQuote]=useState("")
+const [author,setAuthor]=useState("")
+const [run,setRun]=useState(false)
+const hasEffectRun=useRef(false)
+
+let tweet=`https://twitter.com/intent/tweet?text=${encodeURIComponent(quote)}`
+const newQuote=()=>{
+  setRun(prev=>!prev)
+}
+useEffect(()=>{
+
+  if(!hasEffectRun.current){
+    hasEffectRun.current=true;
+    return;
+  }
+    
+    
+    axios.get(url, {headers: {
+    'X-Api-Key': '/XJUfaMN9+D87rJ4TjZiJA==KmHKpQdaevhrI8IV',
+  }})
+  .then((res)=>{
+    setQuote(res.data[0].quote)
+    setAuthor(res.data[0].author)}
+    
+  ).catch((err)=>console.error(err))
+  
+
+  
+
+
+},[run]
+
+)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div id='quote-box'className=''>
+      <div id='text'>{quote}</div>
+      <div id='author'>{author}</div>
+      <button id='new-quote' onClick={newQuote}>newQuote</button>
+      <a href={tweet} target='_blank' id='tweet-quote'>tweetQuote</a>
+      
+
+     </div>
     </div>
   );
 }
